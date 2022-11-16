02dc4ec1c11599c59be4eafe064527a175983a1e
Adam Bonkowski <met8919@gmail.com>
2022-11-16 14:16:27 -0500

    03

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,11 +1,32 @@
 // Your code here
 
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static makeJet() {
+    const newDog = new Dog("Jet");
+    return newDog;
+  }
+
+  changeName(newName) {
+    this.name = newName;
+    return newName;
+  }
+
+  speak(word) {
+    return `${this.name} says ${word}`;
+  }
+}
+
+const d = new Dog("max");
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = Dog;
+  module.exports = Dog;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
